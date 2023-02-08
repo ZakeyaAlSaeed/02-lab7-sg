27df2c06617325d85626a5f0de202390f214a81d
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-02-08 09:55:06 +0300

    readjusted the code with new formatting

--- 5-pub-sg-config.tf
+++ 5-pub-sg-config.tf
@@ -3,12 +3,29 @@ module "public_services_sg" {
   version = "4.17.1"
 
   name        = "public-services-sg"
-  description = "Allow access to public services"
+  description = "Allow access to public services i.e port 80"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp"]
 
-  egress_rules = ["all-all"]
+   ingress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 80
+      protocol    = "tcp"
+      description = "Port80 for Loadbalancer"
+      cidr_blocks = "0.0.0.0/0"
+    }]
+
+    # # Method 1
+    # egress_cidr_blocks = [
+    # {
+    #   from_port   = 0
+    #   to_port     = 0
+    #   protocol    = "-1"
+    #   description = "Allow all outbound traffic"
+    #   cidr_blocks = "0.0.0.0/0"}]
+
+    # Method 2
+    egress_rules = ["all-all"]
 
 }
